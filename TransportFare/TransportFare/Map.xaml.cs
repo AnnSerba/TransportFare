ef5c5693b5211e842277ef12e207776e2331d532
Аня Серба <annserba94@gmail.com>
2016-02-07 21:49:10 +0300

    определение собственных координат и их отображение

--- TransportFare/TransportFare/Map.xaml.cs
+++ TransportFare/TransportFare/Map.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -26,7 +27,6 @@ namespace TransportFare
         {
             this.InitializeComponent();
         }
-
         private void ButtonBack_Click(object sender, RoutedEventArgs e)
         {
             MainMenu mainMenu = Window.Current.Content as MainMenu;
@@ -39,6 +39,19 @@ namespace TransportFare
                 // установка для текущего окна
                 Window.Current.Content = mainMenu;
             }
+        }
+
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            map.MapServiceToken = "mapToken";
+            // получаем инструмент геолокации
+            var geolocator = new Geolocator();
+            //точность геолокации до 150 метров
+            geolocator.DesiredAccuracyInMeters = 150;
+            // получаем позицию
+            var position = await geolocator.GetGeopositionAsync();
+            // установка этой позиции на карте
+            await map.TrySetViewAsync(position.Coordinate.Point, 19);
         }
     }
 }
