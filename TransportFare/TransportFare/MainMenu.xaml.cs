ba86e5cd6cbdc011b62a8581bdc9fa69d3c1f3c4
Аня Серба <annserba94@gmail.com>
2016-02-07 20:51:05 +0300

    Основные элементы

---
+++ TransportFare/TransportFare/MainMenu.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TransportFare
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ButtonToMap_Click(object sender, RoutedEventArgs e)
+        {
+            Map map = Window.Current.Content as Map;
+            // если не создан
+            if (map == null)
+            {
+                // создание 
+                map = new Map();
+
+                // установка для текущего окна
+                Window.Current.Content = map;
+            }
+        }
+
+        private void ButtonToPay_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TransportFare/TransportFare/Map.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Шаблон элемента пустой страницы задокументирован по адресу http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TransportFare
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class Map : Page
+    {
+        public Map()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu mainMenu = Window.Current.Content as MainMenu;
+            // если не создан
+            if (mainMenu == null)
+            {
+                // создание 
+                mainMenu = new MainMenu();
+
+                // установка для текущего окна
+                Window.Current.Content = mainMenu;
+            }
+        }
+    }
+}
